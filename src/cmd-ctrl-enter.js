c052c516f630d2155f8de6065751791b9fdd8197
Dom Christie <christiedom@gmail.com>
2016-06-28 13:22:14 +0100

    Improve formatting

--- src/cmd-ctrl-enter.js
+++ src/cmd-ctrl-enter.js
@@ -1,21 +1,23 @@
 (function($) {
+  var nav = window.navigator;
+  var mac = 'platform' in nav && (/^Mac/).test(nav.platform);
+
   $.fn.cmdCtrlEnter = function(o) {
-
-    var nav = window.navigator,
-        mac = 'platform' in nav && (/^Mac/).test(nav.platform);
-
     if(o === 'which') return mac ? '⌘' : 'Ctrl';
-
     if(o === 'destroy') return this.unbind('keydown.cmdCtrlEnter');
 
     this.filter('textarea').each(function(i, el) {
-      var $el = $(el), $form = $el.parents('form'), $btn = $form.find('[type=submit]');
+      var $el = $(el);
+      var $form = $el.parents('form');
+      var $btn = $form.find('[type=submit]');
 
       $el.on('keydown.cmdCtrlEnter', function(evt) {
-        if(evt.which === 13 && ((evt.ctrlKey && !mac) || evt.metaKey))
+        if(evt.which === 13 && ((evt.ctrlKey && !mac) || evt.metaKey)) {
           if(!$btn.attr('disabled')) $form.submit();
+        }
       });
     });
+
     return this;
   };
 })(jQuery);
